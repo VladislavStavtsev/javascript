c814040a3e750c2a28fbe2e6721d0a3600340496
VLADISLAV STAVTSEV <140553104+VladislavStavtsev@users.noreply.github.com>
2025-06-16 12:38:06 +0300

    1

--- lab1js/lab1.js
+++ lab1js/lab1.js
@@ -20,8 +20,8 @@ form.addEventListener('submit', function (event) {
     ) {
         message = "Вам ещё работать и работать";
     } else if (
-        (gender === "male" && age >= 60 && age < 65) ||
-        (gender === "female" && age >= 55 && age < 60)
+        (gender === "male" && age >= 60 && age <= 64) ||
+        (gender === "female" && age >= 55 && age <= 59)
     ) {
         message = "Скоро пенсия!";
     } else if (
